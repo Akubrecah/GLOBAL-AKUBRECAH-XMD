458fc23459d13dacad719e32a53f62a97b94e020
Akubrecah <73630463+Akubrecah@users.noreply.github.com>
2024-11-01 09:53:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,24 +4,24 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
-global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
-global.location = "Pakistan, Karachi, Karachi" //ur location
+//global.ytname = "YT: Akubrecah Entertainment" //ur yt chanel name
+global.socialm = "GitHub: Akubrecah" //ur github or insta name
+global.location = "Africa, Kenya, Kapenguria" //ur location
 
 //new
-global.botname = '𝙂𝙇𝙊𝘽𝘼𝙇-𝙓𝙈𝘿' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
+global.botname = '𝙂𝙇𝙊𝘽𝘼𝙇-AKUBRECAH-𝙓𝙈𝘿' //ur bot name
+global.ownernumber = ['254791497858',254739926767] //ur owner number, dont add more than one
+global.ownername = '© Akubrecah' //ur owner name
+//global.websitex = "https://www.youtube.com/@GlobalTechInfo"
+//global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
 global.wm = "Global Bot Inc."
-global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-XMD' //script link
+global.botscript = 'https://github.com/Akubrecah/GLOBAL-AKUBRECAH-XMD' //script link
 global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.author = "Akubrecah\n\n+254739926767\n\n+254791497858"
+global.creator = "254791497858@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["254791497858","254739926767"] // Premium User
 
 //channel id
 global.xchannel = {
